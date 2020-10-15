68e58b99a2104710c98a2604fe66218650409476
Ankit Kesar <56121226+mrawk@users.noreply.github.com>
2020-10-15 15:05:30 +0530

    Add files via upload

--- Lab-3/gross.sh
+++ Lab-3/gross.sh
@@ -1,10 +1,14 @@
-echo  "enter the basic salary:"
-read  bsal
-if [  $bsal -lt  100 ];then
-gsal=$((bsal+((bsal/100)*10)))
-echo  "The gross salary : $gsal"
-fi
-if [ $bsal -ge  100 ];then
-gsal=$(((bsal+500)+(bsal/100)*98))
-echo  "the gross salary : $gsal"
-fi
+echo "Enter ur basic salary "
+    read sal
+    if [ $sal -ge 1000 ]
+    then
+         da=`expr $sal \* 50 / 100`
+         ha=`expr $sal  \* 20  / 100`
+         Nsal=`expr $sal + $da + $ha`
+         echo "Basic Salary        $sal "
+         echo "DA  $da "
+         echo "HA $ha "
+         echo "Ur Net Salary is   Rs. $Nsal "
+	else
+        echo "Your Net salary is $sal"
+    fi 
